b9b2b163f552ae078c08aba1ce1e4c7f7b568a5f
unisanta <unisanta@M227-17>
2023-03-02 21:42:12 -0300

    Add project files.

---
+++ ConsoleApp2/Imc.cs
@@ -0,0 +1,57 @@
+﻿
+namespace ConsoleApp2
+{
+    class Imc
+    {
+        public double peso;
+        public double altura;
+        public double resultado;
+        public string resposta;
+        public Imc(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public string imc()
+        {
+            resultado = (double)peso / ((double)altura * (double)altura);
+
+            //return resultado;
+
+            if (resultado <= 20)
+
+            {
+                return resposta = "Abaixo do peso";
+            }
+            else if (resultado <= 25.00)
+            {
+                return resposta = "Peso normal";
+            }
+            else if (resultado <= 30.00)
+            {
+                return resposta = "Sobrepreso";
+            }
+            else if (resultado <= 35.00)
+            {
+                return resposta = "Obesidade grau I";
+            }
+            else if (resultado <= 40.00)
+            {
+                return resposta = "Obesidade grau II";
+            }
+            else if (resultado < 40.00)
+            {
+                return resposta = "Obesidade grau III";
+            }
+
+            return resposta;
+
+        }
+
+
+
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class program
+    {
+        static void Main (string [] args)
+        {
+            Console.Write("Digite sua altura : ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Imc Objetoimc = new Imc(peso,altura);
+
+            Console.WriteLine("Você está: " + Objetoimc.imc()); ;
+        }
+    }
+}
